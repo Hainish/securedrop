f666e9c9d78a94c58ed00f62e299757788f96acf
William Budington <bill@eff.org>
2017-11-06 12:02:14 -0800

    Start redis

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@ set -e
 
 ansible-playbook --connection=local --limit development /securedrop/install_files/ansible-base/securedrop-development.yml --tags securedrop_config
 
+service redis-server start
+
 exec "$@"
 
